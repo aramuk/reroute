b050babef784f36fd2bc78400b05e6e246f0b978
Aditesh Kumar <27091329+aramuk@users.noreply.github.com>
2021-03-08 21:46:27 -0800

    Move to AWS

--- main.py
+++ main.py
@@ -1,54 +1,63 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.8
+
+import os
+import shutil
 
 import matplotlib.pyplot as plt
-import torch
-import torch.nn as nn
-import torch.utils as utils
-from torchvision.models.segmentation import deeplabv3_resnet50
-import torchvision.transforms as transforms
-from torchvision.datasets import Cityscapes
+import numpy as np
+import tensorflow as tf
+import tensorflow_datasets as tfds
 
-def get_dataset(split='train'):
-    return Cityscapes('./cityscapes',
-                    mode='fine',
-                    split=split,
-                    target_type='semantic',
-                    transform=transforms.Compose([
-                        transforms.ToTensor()
-                    ]),
-                    target_transform=transforms.Compose([
-                        transforms.ToTensor()
-                    ]))
+from lib.model import DeepLabV3, download_model
+from lib.visualize import vis_segmentation
 
-def explore(train_set):
-    train_set = get_dataset('train')
-    train_loader = utils.data.DataLoader(train_set, batch_size=20)
-    images, targets = next(iter(train_loader))
-    print(images.shape, targets.shape)
-    images = images.permute(0, 2, 3, 1)
-    targets = targets.permute(0, 2, 3, 1)
-    print(images.shape, targets.shape)
-    fig, ax = plt.subplots(20, 2, figsize=(10, 20))
-    for i in range(20):
-        ax[i][0].imshow(images[i])
-        ax[i][1].imshow(targets[i])
-    plt.show()
 
-def main():
-    test_set = get_dataset('test')
+def save_model(model):
+    image = Image.open(
+        'cityscapes/leftImg8bit/test/berlin/berlin_000032_000019_leftImg8bit.png'
+    )
+    resized_im, batch_seg_map = model.run(image)
+    if tf.executing_eagerly():
+        tf.compat.v1.disable_eager_execution()
 
-    model = deeplabv3_resnet50(pretrained=True)
-    print(model.classifier)
-    image, target = next(iter(test_set))
-
-    output = model(image)
-
-    fig, ax = plt.subplots(1, 3)
-    for i in range(1):
-        ax[i][0].imshow(image)
-        ax[i][1].imshow(output)
-        ax[i][2].imshow(target)
+    save_dir = 'models/mobilenetv2_coco_cityscapes_trainfine'
+    if os.path.exists(save_dir):
+        shutil.rmtree(save_dir)
+    tf.compat.v1.saved_model.simple_save(
+        model.sess, save_dir,
+        {model.INPUT_TENSOR_NAME: tf.convert_to_tensor(resized_im)},
+        {model.OUTPUT_TENSOR_NAME: tf.convert_to_tensor(batch_seg_map)})
+    print('{} model saved to {}'.format(model.__class__, save_dir))
 
 
 if __name__ == '__main__':
-    main()
+    tarball_path = download_model()
+    model = DeepLabV3(tarball_path)
+
+    dataset, info = tfds.load('cityscapes/semantic_segmentation', split='train', with_info=True)
+
+    example = next(iter(dataset))
+    print(example['image_id'])
+    print(example['image_left'])
+    print(example['segmentation_label'])
+
+    tfds.visualization.show_examples(dataset, info, row=3, col=3)
+
+    # fig, ax = plt.subplots(5, 4)
+    # for i, example in enumerate(dataset):
+    #     if i == 5:
+    #         break
+    #     image = example['image_left']
+    #     resized_im, batch_seg_map = model.run(image)
+    #     target = example['segmentation_label']
+    #     # target = np.asarray(Image.fromarray(example['segmentation_label'].numpy).resize(
+    #     #     resized_im.shape, Image.ANTIALIAS))
+    #     # Display
+    #     ax[i][0].imshow(image)
+    #     ax[i][1].imshow(batch_seg_map[0])
+    #     ax[i][2].imshow(target)
+    #     acc = (batch_seg_map[0] == target)
+    #     print(type(target), type(batch_seg_map[0]))
+    #     print(batch_seg_map[0].shape, target.shape, type(acc), acc.shape)
+    #     ax[i][3].imshow(acc)
+    # plt.show()
