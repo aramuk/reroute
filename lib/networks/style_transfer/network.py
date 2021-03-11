8b4e8bfaad984c5a909e66e5f10bb6a152de6e9a
Aditesh Kumar <27091329+aramuk@users.noreply.github.com>
2021-03-11 04:17:02 -0800

    Return feature maps from style transfer model instead of loss

--- lib/networks/style_transfer/network.py
+++ lib/networks/style_transfer/network.py
@@ -13,7 +13,7 @@ from layers import AdaIN, TVLoss
 from losses import content_loss, style_loss
 
 
-def build_model(input_shape=(None, 224, 224, 3), **kwargs):
+def build_model(input_shape=(224, 224, 3), **kwargs):
     # Build layers of model
     encoder = Encoder(**kwargs.get('encoder_kwargs', {}))
     decoder = Decoder(**kwargs.get('decoder_kwargs', {}))
@@ -28,53 +28,55 @@ def build_model(input_shape=(None, 224, 224, 3), **kwargs):
     target = adain([content_features[-1], style_features[-1]])
     output = decoder(target)
     # Compute loss
-    output_encodings = encoder(output)
+    # output_features = encoder(output)
     # loss = tv_loss([style_features, output_encodings, target])
     # Build model
-    model = models.Model(inputs=[content_input, style_input],
-                         outputs=[
-                             output, target, content_features, style_features,
-                             output_encodings
-                         ])
+    model = models.Model(
+        inputs=[content_input, style_input],
+        outputs=[output, target, content_features, style_features])
     return model
 
 
 if __name__ == '__main__':
+    print('Validating style transfer architecture......')
+
     INPUT_SHAPE = (224, 224, 3)
-    EXPECTED_TARGET_SHAPE = (None, 14, 14, 96)
-    EXPECTED_OUTPUT_SHAPE = (None, *INPUT_SHAPE)
+    BATCH_SIZE = 10
+    EXPECTED_TARGET_SHAPE = (BATCH_SIZE, 14, 14, 96)
+    EXPECTED_OUTPUT_SHAPE = (BATCH_SIZE, *INPUT_SHAPE)
 
-    encoder = Encoder()
-    decoder = Decoder()
-    adain = AdaIN()
-
-    content_input = layers.Input(shape=INPUT_SHAPE, name='content_image')
-    style_input = layers.Input(shape=INPUT_SHAPE, name='style_image')
+    model = build_model(input_shape=INPUT_SHAPE)
 
     def has_shape(target, shape):
         return all(dim == target_dim
                    for dim, target_dim in zip(target.shape, shape))
 
-    content_features = encoder(content_input)
+    C = tf.random.uniform((10, 224, 224, 3))
+    S = tf.random.uniform((10, 224, 224, 3))
+
+    output, target, content_features, style_features = model([C, S])
+    
     assert len(
         content_features
     ) == 5, 'Encoder returns {} content feature maps; expected 5'.format(
         len(content_features))
-    style_features = encoder(style_input)
     assert len(
         style_features
     ) == 5, 'Encoder returns {} style feature maps; expected 5'.format(
         len(style_features))
-    target = adain([content_features[-1], style_features[-1]])
     assert has_shape(
         target,
         EXPECTED_TARGET_SHAPE), 'target has shape {}; expected {}'.format(
             target.shape, EXPECTED_TARGET_SHAPE)
-    output = decoder(target)
     assert has_shape(
         output,
         EXPECTED_OUTPUT_SHAPE), 'output has shape {}; expected {}'.format(
             output.shape, EXPECTED_OUTPUT_SHAPE)
-    model = models.Model(inputs=[content_input, style_input], outputs=[output])
-    for var in model.trainable_weights:
-        print(var.name, var.shape)
+
+    encoder = model.get_layer('mobilenetv2_encoder')
+    output_features = encoder(output)
+    assert len(
+        output_features
+    ) == 5, 'Encoder returns {} output feature maps; expected 5'.format(
+        len(output_features))
+    print('SUCCESS: Style transfer architecture produces correct outputs')
